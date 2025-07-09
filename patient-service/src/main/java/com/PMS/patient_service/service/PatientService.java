cbf921b0b823891584013f476ee46c4d38992180
gurashishgill171 <ggill_be19@thapar.edu>
2025-07-09 13:32:50 +0530

    setup + get-patient endpoint

---
+++ patient-service/src/main/java/com/PMS/patient_service/service/PatientService.java
@@ -0,0 +1,25 @@
+package com.PMS.patient_service.service;
+
+import com.PMS.patient_service.dto.PatientResponseDTO;
+import com.PMS.patient_service.mapper.PatientMapper;
+import com.PMS.patient_service.model.Patient;
+import com.PMS.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(patient -> PatientMapper.toDTO(patient)).toList();
+        return patientResponseDTOS;
+    }
+}
