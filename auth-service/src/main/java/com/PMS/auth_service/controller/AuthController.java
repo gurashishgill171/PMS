8bcc160d6b5b5205120766595bb6eb90540fef61
gurashishgill171 <ggill_be19@thapar.edu>
2025-08-03 12:05:22 +0530

    Auth service + Login Endpoint

---
+++ auth-service/src/main/java/com/PMS/auth_service/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.PMS.auth_service.controller;
+
+import com.PMS.auth_service.dto.LoginRequestDto;
+import com.PMS.auth_service.dto.LoginResponseDto;
+import com.PMS.auth_service.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequest) {
+        Optional<String> tokenOptional = authService.authenticate(loginRequest);
+
+        if(tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+}
